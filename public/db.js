f789cb3d6841d852da39ed74a2b47ace239d94b8
JP-Grace <gracejohnpaul200@gmail.com>
2021-04-22 01:14:25 -0400

    checkDatabase() now created; accesses IDB during onSuccess()

--- public/db.js
+++ public/db.js
@@ -3,6 +3,7 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
+  console.log("UPGRADE");
    // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
@@ -10,6 +11,7 @@ request.onupgradeneeded = function(event) {
 
 request.onsuccess = function(event) {
   db = event.target.result;
+  console.log(db);
 
   // check if app is online before reading from db
   if (navigator.onLine) {
@@ -23,4 +25,13 @@ request.onerror = function(event) {
 
 function saveRecord(record) {
   console.log(record);
+}
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
 }
